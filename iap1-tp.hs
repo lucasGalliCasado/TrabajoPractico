b15c6116b658af123e2b87ee2122ca04ee6070a5
berny3101 <132222086+berny3101@users.noreply.github.com>
2023-05-15 04:50:08 -0300

    Update iap1-tp.hs

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -48,11 +48,12 @@ nombresDeUsuarios = undefined
 
 
 --- | Ejericio 2 |-----------------------------------------------------------------------------------------------------------------------------
+-- PASA EL TEST CATEDRA
+
 -- Aca use el que estaba en el codigo de jos, pero modifique las condiciones de pruebaRelacion pq por lo menos a mi me daba error
 
 
--- Recibe como parametros una RedSocial y un Usuario de la misma. Devuelve una lista contendiendo a todos los usuarios de la red con
--- los cuales el Usuario ingresado tiene una relacion de amistad
+-- Recibe como parametros una RedSocial y un Usuario de la misma. Devuelve una lista contendiendo a todos los usuarios de la red con los cuales el Usuario ingresado tiene una relacion de amistad
 amigosDe :: RedSocial -> Usuario -> [Usuario]
 amigosDe (us,rs,ps) u = pruebaRelacion rs us u 
 
@@ -63,10 +64,16 @@ pruebaRelacion rs (us:uss) u | (pertenece (us, u) rs) || (pertenece (u, us) rs) 
                              | otherwise = pruebaRelacion rs uss u
 
 --- | Ejericio 3 |-----------------------------------------------------------------------------------------------------------------------------
--- describir qué hace la función: .....
+-- PASA EL TEST CATEDRA
+
+-- Comente esta pq en teoria hay un requiere que evita toda la condicion
+-- cantidadDeAmigos :: RedSocial -> Usuario -> Int
+-- cantidadDeAmigos red u  | redSocialValida red && usuarioValido u && pertenece u (usuarios red) = longitud (amigosDe red u)
+--                         | otherwise = 0
+
+-- Devuelve cantidad de amigos que tiene un usuario en una red social
 cantidadDeAmigos :: RedSocial -> Usuario -> Int
-cantidadDeAmigos red u  | redSocialValida red && usuarioValido u && pertenece u (usuarios red) = longitud (amigosDe red u)
-                        | otherwise = 0
+cantidadDeAmigos red u = longitud (amigosDe red u)
 
 
 
@@ -90,22 +97,21 @@ publicacionesDe = undefined
 
 
 --- | Ejericio 7 |-----------------------------------------------------------------------------------------------------------------------------
+-- PASA EL TEST CATEDRA
 
--- describir qué hace la función: .....
+-- Indica las publicaciones que le gustan a un Usuario
 publicacionesQueLeGustanA :: RedSocial -> Usuario -> [Publicacion]
-publicacionesQueLeGustanA red u | not (redSocialValida red) || not (usuarioValido u) || not (pertenece u (usuarios red)) = []
-                                | otherwise = publicacionesQueLeGustanARec red u (publicaciones red) []
+publicacionesQueLeGustanA red u = filtrarPublicaciones red u (publicaciones red)
 
-publicacionesQueLeGustanARec :: RedSocial -> Usuario -> [Publicacion] -> [Publicacion] -> [Publicacion]
-publicacionesQueLeGustanARec _ _ [] res = sinRepetidos2 res
-publicacionesQueLeGustanARec red u (p:ps) res   | pertenece u (likesDePublicacion p) && pertenece p (publicaciones red) = publicacionesQueLeGustanARec red u ps (p:res)
-                                                | otherwise = publicacionesQueLeGustanARec red u ps res
+-- Selecciona las publicaciones las cuales tienen el like del usuario
+filtrarPublicaciones :: RedSocial -> Usuario -> [Publicacion] -> [Publicacion]
+filtrarPublicaciones _ _ [] = []
+filtrarPublicaciones red u (p:ps) | leGustaPublicacion u p = p : filtrarPublicaciones red u ps
+                                  | otherwise = filtrarPublicaciones red u ps
 
-sinRepetidos2 :: Eq a => [a] -> [a]
-sinRepetidos2 [] = []
-sinRepetidos2 (x:xs)
-  | pertenece x xs = sinRepetidos2 xs
-  | otherwise   = x : sinRepetidos2 xs
+-- Indica si un usuario le dio like a una publicacion
+leGustaPublicacion :: Usuario -> Publicacion -> Bool
+leGustaPublicacion u (_, _, likes) = pertenece u likes
 
 
 --- | Ejericio 8 |-----------------------------------------------------------------------------------------------------------------------------
